43ea3fcfb5c3cbbba94a4db8c8176cb7385a33f5
DeathHound <acraig176@hotmail.com>
2021-11-06 16:11:24 +0000

    Export Command and Event

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = {
     Client: require("./lib/Client.js"),
+    Command: require("./lib/Structures/Command.js"),
+    Event: require("./lib/Structures/Event.js"),
     HandlerClient: require("./lib/Client.js"),
     Version: require("./package.json").version,
     Util: require("./lib/Util.js")
